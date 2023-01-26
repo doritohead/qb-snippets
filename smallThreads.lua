a5525d91c2f1800383a63142cf4f023afb8fdb63
doritohead <81892751+doritohead@users.noreply.github.com>
2023-01-26 03:03:38 +0000

    Update smallThreads.lua

--- smallThreads.lua
+++ smallThreads.lua
@@ -1,16 +1,16 @@
 ---Ragdoll when jumping
 Config.RagdollChance = 0.5
-Citizen.CreateThread(function()
+CreateThread(function()
     while true do
-        Citizen.Wait(100)
+        Wait(100)
         if IsPedOnFoot(PlayerPedId()) and not IsPedSwimming(PlayerPedId()) and (IsPedRunning(PlayerPedId()) or IsPedSprinting(PlayerPedId())) and not IsPedClimbing(PlayerPedId()) and IsPedJumping(PlayerPedId()) and not IsPedRagdoll(PlayerPedId()) then
             local Result = math.random()
             if Result < Config.RagdollChance then 
-                Citizen.Wait(600)
+                Wait(600)
                 ShakeGameplayCam('SMALL_EXPLOSION_SHAKE', 0.08)
                 SetPedToRagdoll(PlayerPedId(), math.random(2500, 5000), 1, 2)
             else
-                Citizen.Wait(2000)
+                Wait(2000)
             end
         end
     end
@@ -18,7 +18,7 @@ end)
 
 --stop hat/glases falling off
 local lastped = nil
-Citizen.CreateThread(function()
+CreateThread(function()
     while true do
         if PlayerPedId() ~= lastped then
             lastped = PlayerPedId()
